d936b85faa4e3a6617f8a34405672b02e8887b21
Brianna Escobar <briannaescobar6@gmail.com>
2023-05-04 15:35:48 -0400

    node-api-ec2-aws

---
+++ index.js
@@ -0,0 +1,9 @@
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.get('/', (req,res)=> res.send('Welcome to my API'))
+app.listen(4000,()=> console.log('API running'))
